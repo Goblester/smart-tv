4a8d64a5658fee41e24a6423f2c20c6ff1ac7ce2
Goblester <goblin829@gmail.com>
2021-08-02 01:55:38 +0300

    JSX-done

--- src/features/LeftPannel/SuccessScreen/SuccessScreen.tsx
+++ src/features/LeftPannel/SuccessScreen/SuccessScreen.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 export const SuccessScreen: React.FC = () => {
     return (
         <div>
-
+            <h2>ЗАЯВКА ПРИНЯТА</h2>
+            <p>Держите телефон под рукой. Сокор с Вами свяжется менеджер.</p>
         </div>
     )
 }

bf14ac052a122f7e3a4ad58cc9f5822f3af13291
Goblester <goblin829@gmail.com>
2021-08-02 02:03:31 +0300

    mark up-done

--- src/features/LeftPannel/SuccessScreen/SuccessScreen.tsx
+++ src/features/LeftPannel/SuccessScreen/SuccessScreen.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import st from './SuccessScreen.module.scss';
 
 
 export const SuccessScreen: React.FC = () => {
     return (
-        <div>
-            <h2>ЗАЯВКА ПРИНЯТА</h2>
-            <p>Держите телефон под рукой. Сокор с Вами свяжется менеджер.</p>
+        <div className={st.container}>
+            <h2>ЗАЯВКА <br/> ПРИНЯТА</h2>
+            <p>Держите телефон под рукой. <br/> Сокро с Вами свяжется наш менеджер.</p>
         </div>
     )
 }

f65ac92454ae840a16e3bf96722c74f550721e3a
Goblester <goblin829@gmail.com>
2021-08-01 17:49:54 +0300

    mark up-done

--- src/features/Banner/Banner.tsx
+++ src/features/Banner/Banner.tsx
@@ -1,15 +1,19 @@
+import Button from '../../components/Button/Button';
 import qr from './../../assets/images/qr-code.png'
+import st from './Banner.module.scss';
 
 export const Banner = () => {
     return (
-        <div>
+        <div className={st.container}>
             <p>ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША! ПОДАРИТЕ ЕМУ СОБАКУ</p>
             <figure>
                 <img src={qr}
-                     alt={''} />
-                    <figcaption>Сканируйте QR-код или нажмите ОК</figcaption>
+                     alt={''}/>
+                <figcaption>Сканируйте QR-код или нажмите ОК</figcaption>
             </figure>
-            <button>OK</button>
+            <div className={st.btnContainer}>
+                <Button className={st.button}>OK</Button>
+            </div>
         </div>
     )
 }
